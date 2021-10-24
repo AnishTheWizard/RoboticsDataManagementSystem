cae612645a44f13f44eafb90ce830be2ff3b8e9d
anishthewizard <chunumunnu@gmail.com>
2021-10-24 12:51:57 -0400

    init freeze frame

---
+++ DataManagement.py
@@ -0,0 +1,124 @@
+"""Library to Manage the Data Retrieved by the Scouting System"""
+
+import ast
+import csv
+import json
+from typing import TextIO
+
+from DataErrors import *
+
+__author__ = "Anish Chandra"
+__version__ = "1.0"
+
+
+class DataHandler(object):
+    def __init__(self, location: str, openType: str) -> None:
+        try:
+            self.location = location
+            self.openType = openType
+            self.file = open(location, openType)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {location} does not exist")
+
+    def _close(self) -> None:
+        self.file.flush()
+        self.file.close()
+
+    def _save_file(self, fileText) -> None:
+        output = open(self.location, "w")
+        output.write(fileText)
+        output.flush()
+        output.close()
+
+    def _open_new_file(self, location: str, openType: str) -> None:
+
+        try:
+            self.location = location
+            self.openType = openType
+            self.file = open(location, openType)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {location} does NOT exist")
+
+    def _get_file(self) -> TextIO:
+        return self.file
+
+
+class JSONHandler(DataHandler):
+    def __init__(self, location: str) -> None:
+        super().__init__(location, "r")
+        try:
+            self.json = json.loads(super()._get_file().read())
+        except json.decoder.JSONDecodeError:
+            raise DuplicationError(f"File at location {location} has a duplicate")
+        # except more errors in formatting here and create more in DataErrors.py such as
+        # except ValueError:
+        #   raise SomeNewError()
+
+    def change_file(self, location: str) -> None:
+        super()._open_new_file(location, 'r')
+        try:
+            self.json = json.loads(super()._get_file().read())
+        except json.decoder.JSONDecodeError:
+            raise DuplicationError(f"File at location {location} has a duplicate")
+
+    def get_file(self):
+        return self.json
+
+    def set_file(self, newJSON: str):
+        self.json = json.loads(newJSON)
+
+    def save_file(self) -> str:
+        super()._save_file(json.dumps(self.json))
+        return json.dumps(self.json)
+
+
+class CSVHandler(DataHandler):
+    def __init__(self, location: str, toRead: bool) -> None:
+        self.toRead = toRead
+        super().__init__(location, "r" if toRead else "w")
+        self.csv = csv.reader(self.file) if toRead else csv.writer(self.file)
+
+    def write_row(self, row: list) -> None:
+        self.csv.writerow(row)
+
+    def get_file(self):
+        return self.csv
+
+    def save_file(self) -> None:
+        super()._close()
+
+
+class ScoutingJSONReformatter(JSONHandler):
+    formattedJSON = dict()
+
+    def __init__(self, location: str, labelOrder: list) -> None:
+        self.labelOrder = labelOrder
+        super().__init__(location)
+
+    def add_labels(self) -> None:
+        if type(super().get_file()) is not list or len(self.labelOrder) != len(super().get_file()):
+            raise ImproperlyFormattedJSONError(f"File at location {self.location} may already be formatted")
+
+        rawMatchData = self.json
+        for i in range(len(rawMatchData)):
+            self.formattedJSON[self.labelOrder[i]] = rawMatchData[i]
+        self.json = self.formattedJSON
+
+    def export_format(self) -> None:
+        super().save_file()
+
+
+class CSVCompositor(CSVHandler):
+
+    def __init__(self, location: str, headers: list) -> None:
+        super().__init__(location, False)
+        self.csv.writerow(headers)
+        self.lines = 0
+
+    def convert_json_to_csv(self, json: str) -> None:
+        json = ast.literal_eval(json)
+        for data in json:
+            self.csv.writerow(list(data.values()))
+
+    def save_file(self) -> None:
+        super().save_file()
---
+++ Main.py
@@ -0,0 +1,107 @@
+"""Main Code Compiling and Handling All Parts of Data"""
+from DataManagement import *
+from DataErrors import *
+import sys
+import os
+import json
+import logging
+
+__author__ = "Anish Chandra"
+__version__ = "1.0"
+
+
+def compile_all_team_data(targetLocation: str) -> None:
+    compiledJSON = open(targetLocation, "w")
+    compiledJSON.write("[")
+
+    for index, file in enumerate(os.listdir()):
+        if file.find(".json") != -1 and file.find("-") != -1:
+            with open(file, "r") as f:
+                compiledJSON.write(f.read())
+
+            if index <= len(os.listdir()) - 2:
+                compiledJSON.write(", \n")
+
+    compiledJSON.write("]")
+
+
+if __name__ == '__main__':
+    # Setup
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+    # Get all configuration parameters
+    args = sys.argv[1:]
+
+    working_directory = str()
+    target_json = str()
+    target_csv = str()
+    label_order = list()
+
+    if len(args) == 4:
+        working_directory = args[0]
+        target_csv = args[1]
+        label_order = args[2]
+
+    elif len(args) == 1:
+        try:
+            config = json.loads(open(args[0], "r").read())
+            working_directory = config["Working_Directory"]
+            target_json = config["Target_JSON"]
+            target_csv = config["Target_CSV"]
+            label_order = config["Label_Order"]
+        except FileNotFoundError:
+            logging.critical(f"Config File Does Not Exist")
+            exit(ExitDueToFailure("This Code will Exit"))
+        except json.decoder.JSONDecodeError:
+            logging.critical(f"Config File is not formatted properly")
+            exit(ExitDueToFailure("This Code will Exit"))
+
+    else:
+        logging.critical(f"Incorrect Number of Arguments Passed")
+        exit(ExitDueToFailure("This Code will Exit"))
+
+    # Check if all parameters are correct
+    if None in [working_directory, target_json, target_csv, label_order]:
+        logging.critical(f"One of the arguments is of NoneType")
+        exit(ExitDueToFailure("This Code will Exit"))
+
+    # Set working directory
+    os.chdir(working_directory)
+
+    # Find files to reformat
+    filesToReformat = list()
+    for file in os.listdir():
+        if file.find(".json") != -1 and file.find("-") != -1:
+            filesToReformat.append(file)
+
+    logging.debug(f"Files to reformat: {str(filesToReformat)}")
+
+    # Reformat Files into Labeled Data
+    if len(filesToReformat) == 0:
+        logging.critical(f"There are no files to reformat in the directory {os.getcwd()}")
+        exit(1)
+
+    reformatter = ScoutingJSONReformatter(filesToReformat[0], label_order)
+
+    for file in filesToReformat:
+        try:
+            reformatter.change_file(file)
+            reformatter.add_labels()
+            reformatter.save_file()
+            logging.info(f"Successfully Reformatted File {file}")
+        except ImproperlyFormattedJSONError:
+            logging.warning(f"File {file} may have already been formatted")
+        except DuplicationError as dup:
+            logging.warning(f"File {file} has a duplicate or is corrupted\n{dup}")
+
+    # Compile all Data into one JSON
+    compile_all_team_data(target_json)
+    logging.info("Successfully Created Compiled JSON")
+
+    # Compile all Data into one CSV
+
+    compositor = CSVCompositor(target_csv, label_order)
+    compositor.convert_json_to_csv(json.dumps(json.loads(open(target_json, "r").read())))
+    compositor.save_file()
+    logging.info("Successfully Created Compiled CSV")
+    logging.info("Done")
